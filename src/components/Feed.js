ce78b88c4a75134c953ab5632beb3a1740d46653
Scott Nelson <snelson@instructors.2u.com>
2022-10-19 23:21:47 -0700

    finished layout for Feed and Post components, fixed positioning issue with Sidebar component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,46 @@
 import { Box } from "@mui/material";
+import Post from "./Post";
+
+import bridge from "../images/bridge.jpg";
+import field from "../images/field.jpg";
+import building from "../images/building.jpg";
+import morning from "../images/morning.jpg";
+import boats from "../images/boats.jpg";
 
 const Feed = () => {
   return (
-    <Box flex={4} p={2}>Feed</Box>
-  )
-}
-export default Feed
+    <Box flex={4} p={2}>
+      <Post
+        content={"lorem ipsum"}
+        title="John Doe"
+        date="October 18, 2022"
+        imageUrl={bridge}
+      />
+      <Post
+        content={"lorem ipsum"}
+        title="Unknown"
+        date="October 17, 2022"
+        imageUrl={building}
+      />
+      <Post
+        content={"lorem ipsum"}
+        title="Scott"
+        date="October 17, 2022"
+        imageUrl={field}
+      />
+      <Post
+        content={"lorem ipsum"}
+        title="John Doe"
+        date="October 16, 2022"
+        imageUrl={morning}
+      />
+      <Post
+        content={"lorem ipsum"}
+        title="Unknown"
+        date="October 14, 2022"
+        imageUrl={boats}
+      />
+    </Box>
+  );
+};
+export default Feed;
---
+++ src/components/Post.js
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Avatar,
+  Typography,
+  IconButton,
+  Checkbox
+} from "@mui/material";
+
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+
+const Post = ({title, date, content, imageUrl, imageDescription}) => {
+  return (
+    <Card sx={{margin: 3}}>
+      <CardHeader avatar={<Avatar  />} title={title} subheader={date} />
+      <CardMedia component="img" height="20%" image={imageUrl} alt={imageDescription} />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+            {content}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton aria-label="like this post">
+            <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{color: "red"}} />} />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+export default Post;
